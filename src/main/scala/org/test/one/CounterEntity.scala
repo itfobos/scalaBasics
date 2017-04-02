63b673f6529a584c60a555fd2501d14b9c5a9605
Ilya Turukin <turukin@gmail.com>
2017-04-02 19:37:06 +0700

    Made a case class.

---
+++ src/main/scala/org/test/one/CounterEntity.scala
@@ -0,0 +1,4 @@
+package org.test.one
+
+case class CounterEntity(counter: Int = 0) {
+}
--- src/main/scala/org/test/one/ScalaApp.scala
+++ src/main/scala/org/test/one/ScalaApp.scala
@@ -3,10 +3,10 @@ package org.test.one
 
 object ScalaApp {
   def main(args: Array[String]): Unit = {
-    val testClass: TestClass = new TestClass(8)
-    val repResult: String = testClass.addFromCounter(repAmount = 4, message = "Basis")
+    val testClassInstance = CounterEntity(8)
 
-    println(repResult)
+    testClassInstance == CounterEntity(8)
 
+    println(testClassInstance)
   }
 }
