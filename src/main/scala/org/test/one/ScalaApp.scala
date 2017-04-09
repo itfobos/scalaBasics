d41ba7fb856b2a73824cbbd10abc77c50cbd2f5e
Ilya Turukin <turukin@gmail.com>
2017-04-09 17:52:25 +0700

    Set check

--- src/main/scala/org/test/one/ScalaApp.scala
+++ src/main/scala/org/test/one/ScalaApp.scala
@@ -3,14 +3,9 @@ package org.test.one
 
 object ScalaApp {
   def main(args: Array[String]): Unit = {
-    var list = List[Int](1, 4, 5)
-    list = list :+ 4
+    var set: Set[Int] = Set(1, 444, 44, 444, 8)
 
-    var arr: Array[Int] = Array(1, 1, 1, 1)
-    arr = 0 +: arr :+ 5
-    arr.foreach(elem => print(elem + " "))
-    println()
-
-    arr.distinct.foreach(elem => print(elem + " "))
+    println(set(4))
+    println(set.toList)
   }
 }
