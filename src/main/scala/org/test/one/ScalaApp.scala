9830599da1d1a6deede11cf91498758f7e4b2711
Ilya Turukin <turukin@gmail.com>
2017-04-09 17:07:42 +0700

    Lists and arrays checking

--- src/main/scala/org/test/one/ScalaApp.scala
+++ src/main/scala/org/test/one/ScalaApp.scala
@@ -3,10 +3,14 @@ package org.test.one
 
 object ScalaApp {
   def main(args: Array[String]): Unit = {
-    var counterEntity = CounterEntity(2)
-    println(counterEntity.counter)
+    var list = List[Int](1, 4, 5)
+    list = list :+ 4
 
-    counterEntity = counterEntity.copy(counter = counterEntity.counter * 2)
-    println(counterEntity.counter)
+    var arr: Array[Int] = Array(1, 1, 1, 1)
+    arr = 0 +: arr :+ 5
+    arr.foreach(elem => print(elem + " "))
+    println()
+
+    arr.distinct.foreach(elem => print(elem + " "))
   }
 }
